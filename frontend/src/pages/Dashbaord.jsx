71d69d81367276aeaf8473706d7b780ec26b8bb1
anjaliofficial <bistaanjali1415@gmail.com>
2025-11-06 17:15:54 +0545

    changes

---
+++ frontend/src/pages/Dashbaord.jsx
@@ -0,0 +1,10 @@
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>🏏 Women’s World Cup 2025 Dashboard</h1>
+      <p>Overview of teams, players, stats & predictions.</p>
+    </div>
+  );
+};
+
+export default Dashboard;
